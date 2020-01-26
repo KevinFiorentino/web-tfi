f1bbec886de1f4f53bd25e8fbe4b525f8ecbc4fe
KevinFiorentino <kevinfiorentino@hotmail.com>
2020-01-26 11:21:18 -0300

    módulo React

--- js/react-script.js
+++ js/react-script.js
@@ -2,10 +2,11 @@ class FormPronostico extends React.Component {
     render() {
         return (
 			<div id="form-pronostico">
-				<input class="form-control input-react" type="text" name="buscarCiudad" placeholder="Ingrese una ciudad" />
-				<button class="btn btn-success btn-react">Consultar</button>
+				<input class="form-control input-react" type="text" name="valueCiudad" 
+					onChange={this.props.handleChangeInput} value={this.props.value} placeholder="Ingrese una ciudad" />
+				<button class="btn btn-success btn-react" onClick={this.props.handleClimaClick} >Consultar</button>
 				<div class="error-ciudad">
-					<p>Error</p>
+					<p>{this.props.error}</p>
 				</div>					
 			</div>
         )
@@ -17,7 +18,7 @@ class PronosticoHoy extends React.Component {
         return (
 			<div class="pronostico-hoy">
 				<p>{this.props.ciudad}</p>
-				<img class="img-pronostico" src="{this.props.icon}" />
+				<img class="img-pronostico" src={this.props.icon} />
 				<p>{this.props.temp}°</p>
 				<p>{this.props.descrip}</p>	
 			</div>	
@@ -56,7 +57,14 @@ class PronosticoInfo extends React.Component {
 class AppClima extends React.Component {
 	constructor(props) {
 		super(props);
+
+		var meses = new Array ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+		var f = new Date()
+		var fecha = f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear()
+		
 		this.handleClimaClick = this.handleClimaClick.bind(this);
+		this.handleChangeInput = this.handleChangeInput.bind(this);
+
 		this.state = {
   			descrip: '.....',
   			icon: './assets/images/icons/clima_default.png',
@@ -71,31 +79,81 @@ class AppClima extends React.Component {
   			sunset: '--:--',
   			id: 0,
   			city: '---',
-  			fecha: '---',
+  			fecha: fecha,
+
+  			value: '',
 		};
 	}
 
-	handleClimaClick() {
-		fetch("http://api.openweathermap.org/data/2.5/weather?q=Bogota&units=metric&appid=f3f376b99fe63334a561bad62acb4f94")
+	handleClimaClick(event) {
+		fetch("http://api.openweathermap.org/data/2.5/weather?q=" + this.state.value + "&units=metric&appid=f3f376b99fe63334a561bad62acb4f94")
 			.then(res => res.json())
 			.then(
 				(response) => {
-					console.log(response.main.temp)
+
+					var sunrise = new Date(response.sys.sunrise * 1000)
+					var sunrise_hours = sunrise.getHours()
+					var sunrise_minutes = "0" + sunrise.getMinutes()
+
+					var sunset = new Date(response.sys.sunset * 1000)
+					var sunset_hours = sunset.getHours()
+					var sunset_minutes = "0" + sunset.getMinutes()
+
 					this.setState ({
+						descrip: getDescription(response.weather[0].icon),
+						icon: './assets/images/icons/' + response.weather[0].icon + '.svg',
 						temp: response.main.temp,
+			  			min: response.main.temp_min,
+			  			max: response.main.temp_max,
+			  			press: response.wind.speed,
+			  			hum: response.main.humidity,
+			  			vis: response.visibility,
+			  			wind: response.wind.speed,
+			  			sunrise: sunrise_hours + ':' + sunrise_minutes.substr(-2),
+			  			sunset: sunset_hours + ':' + sunset_minutes.substr(-2),
+			  			id: response.id,
+			  			city: response.name,
 					});
 				},
 				(error) => {
-					console.log("error")
+					console.log("err")
+					this.setState ({
+			  			descrip: '.....',
+			  			icon: './assets/images/icons/clima_default.png',
+			  			temp: '--',
+			  			temp_min: '--',
+			  			temp_max: '--',
+			  			press: '-',
+			  			hum: '-',
+			  			vis: '-',
+			  			wind: '-',
+			  			sunrise: '--:--',
+			  			sunset: '--:--',
+			  			id: 0,
+			  			city: '---',
+
+			  			error: "Ciudad no encontrada",
+					});
 				}
 			)
+	}
+
+	handleChangeInput(event) {
+		this.setState({
+			value: event.target.value
+		});
 	}
 
 	render() {
 		return (
 			<React.Fragment>
 				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-					<FormPronostico />
+					<FormPronostico 
+						handleClimaClick = {this.handleClimaClick}
+						error = {this.error}
+						value = {this.state.value}
+						handleChangeInput = {this.handleChangeInput}
+					/>
 				</div>
 
 				<div class="offset-lg-1 offset-md-1 offset-sm-1 offset-xs-1"></div>
@@ -105,16 +163,24 @@ class AppClima extends React.Component {
 
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
 							<PronosticoHoy 
-								ciudad={this.state.city}
-								icon={this.state.icon}
-								temp={this.state.temp}
-								descrip={this.state.descrip}
+								ciudad = {this.state.city}
+								icon = {this.state.icon}
+								temp = {this.state.temp}
+								descrip = {this.state.descrip}
 							/>
 						</div>
 
 						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 							<PronosticoInfo 
-								
+								fecha = {this.state.fecha}
+								min = {this.state.min}
+								max = {this.state.max}
+								sunrise = {this.state.sunrise}
+								sunset = {this.state.sunset}
+								hum = {this.state.hum}
+								wind = {this.state.wind}
+								vis = {this.state.vis}
+								press = {this.state.press}
 							/>		
 						</div>
 
@@ -122,6 +188,38 @@ class AppClima extends React.Component {
 				</div>
 			</React.Fragment>
 		);
+	}
+}
+
+function getDescription(icon) {
+	switch(icon) {
+		case '01d': case '01n': {
+			return "Cielo despejado";
+		};break;
+		case '02d': case '02n': {
+			return "Algunas nubes";
+		};break;
+		case '03d': case '03n': {
+			return "Nublado";
+		};break;
+		case '04d': case '04n': {
+			return "Nublado";
+		};break;
+		case '09d': case '09n': {
+			return "Probabilidad de lluvias";
+		};break;
+		case '10d': case '10n': {
+			return "Lluvioso";
+		};break;
+		case '11d': case '11n': {
+			return "Tormenta eléctrica";
+		};break;
+		case '13d': case '13n': {
+			return "Nevadas";
+		};break;
+		case '50d': case '50n': {
+			return "Niebla";
+		};break;
 	}
 }
 
